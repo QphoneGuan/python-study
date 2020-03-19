f03403cbe026416a463981d1d8e898685ba85973
Qphone Guan <34728265+QphoneGuan@users.noreply.github.com>
2020-03-19 22:28:40 +0800

    Update ali.py

--- ali.py
+++ ali.py
@@ -26,6 +26,39 @@ c = "本月的营业额是%.1f元人民币，比上个月上涨了%s%d"%(13400.2
 print(c) #输出的结果是：本月的营业额是13400.2元人民币，比上个月上涨了%25
 d = f'热烈欢迎{a}{b}莅临指导' #输出的结果就是把定义过的a和b带入这个字符串
 
+#lesson 26-copy string
+a = 'welcome to China！ '
+b = a * 3 #print(b) 输出welcome to China! welcome to China! welcome to China! 
+
+#lesson 27-bool and None
+#布尔值只有两种：True真，False假，两者都是int型，True表示1，False表示0.可以进行计算。
+#空值只有一种：None
+#Attention:True,False and None一定要区分大小写，否则会报错！
+
+#lesson 28-check type
+#type()函数用来返回字符类型，返回值需要print()出来方可显示
+print(type("hello")) #<class 'str'>
+print(type(123)) #<class 'int'>
+print(type(123.32)) #<class 'float'>
+print(type(False)) #<class 'bool'>
+print(type(None)) #<class 'NoneType'>
+
+#lesson 29-objective
+#Python是一种面向对象的语言，CPU通过调取内存中的数据进行计算，而这些数据就是放在内存中的某些区域的。
+#这些数据就是对象。
+
+#lesson 30-structure of objective
+#对象的数据结构包括三个部分：
+  1##id（标识），指数据的编号，在Cpython中是数据在内存中的地址，一经创建不可改变！
+  id()#函数返回标识值 #print(id('hello'))打印的是一串地址数据
+  2##type（类型），指数据的具体类型，例如str,float,bool,None,int等等，一经创建不可改变！
+  type()#函数返回类型#print(type(True))打印结果为：<class 'bool'>
+  3##value（值），指数据的具体内容，例如：123，"hello",True等等，一般不可改变，存在特例，以后再说。
+
+#lesson 31-variable and objective
+
+  
+
 
 
 
